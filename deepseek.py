bb7976aefb7920e7773fe7ae33b944fffab52481
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2025-04-26 22:13:01 +0200

    ROPE implemented

---
+++ deepseek.py
@@ -0,0 +1,47 @@
+class MLA(nn.Module):
+    """ 
+    Multi-Headed Attention Layer (MLA).
+    
+    Attributes: 
+        dim (int): Dimensionality of the input features.
+        n_heads (int): Number of attention heads.
+        n_local_heads (int): Number of local attention heads for distributed systems. (for doing computations on different/multiple GPUs, so you can split them on each GPU)
+        q_lora_rank (int): Rank for low-rank query projection.
+        kv_lora_rank (int): Rank for low-rank key/value projection.
+        qk_nope_head_dim (int): Dimensionality of non-positional query/key projections.
+        qk_rope_head_dim (int): Dimensionality of rotary-positional query/key projections.
+        qk_head_dim (int): Total dimensionality of query/key projections.
+        v_head_dim (int): Dimensionality of value projections.
+        softmax_scale (float): Scaling factor for softmax in attention computation.
+    """
+    
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.dim = args.dim
+        self.n_heads = args.n_heads
+        self.n_local_heads = args.n_heads // world_Size # world_size is number of GPUs you have
+        self.q_lora_rank = args.q_lora_rank
+        self.kv_lora_rank = args.kv_lora_rank
+        self.qk_nope_head_dim = args.qk_nope_head_dim
+        self.qk_rope_head_dim = args.qk_rope_head_dim
+        self.qk_head_dim = args.qk_nope_head_dim + args.qk_rope_head_dim
+        self.v_head_dim = args.v_head_dim
+        
+        if self.q_lora_rank == 0: # if we don't want to use the things invented in the paper, the latent representations
+            self.wq = ColumnParallelLinear(self.dim, self.n_heads * self.qk_head_dim)
+            # the column parallel liner will act the same if we have one GPU, but if we have multiple GPUs generating a vector, it will split and generate part of that vector on each GPU
+        else: # else if we have lora rank and we want to use the architecture presented by deepseek (ROPE)
+            self.wq_a = Linear(self.dim, self.q_lora_rank)
+            self.q_norm = RMSNorm(self.q_lora_rank) # normalise values so theyre not too varied
+            self.wq_b = ColumnParallelLinear(self.q_lora_rank, self.n_heads * self.qk_head_dim)
+        self.wkv_a = Linear(self.dim, self.kv_lora_rank + self.qk_rope_head_dim)
+        self.kv_norm = RMSNorm(self.kv_lora_rank)
+        self.wkv_b = ColumnParallelLinear(self.kv_lora_rank, self.n_heads * (self.qk_nope_head_dim + self.v_head_dim))
+        self.wo = RowParallelLinear(self.n_heads * self.v_head_dim, self.dim)
+        self.softmax_scale = self.qk_head_dim ** -0.5
+        if args.max_seq_len > args.original_seq_len:
+            mscale = 0.1 * args.mscale * math.log(args.rope_factor) + 1.0
+            self.softmax_scale = self.softmax_scale * mscale * mscale
+        
+        # KV CACHE
+            
