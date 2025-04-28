272719f5eb7beb0fe68a728847aa9fc8f3d712ed
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2025-04-28 10:05:44 +0200

    implemented MLA Forward

--- deepseek.py
+++ deepseek.py
@@ -51,3 +51,59 @@ class MLA(nn.Module):
             # the implementation is deepseeks
             self.register_buffer("kv_cache", torch.zeros(args.max_batch_size, args.max_seq_len, self.n_local_heads, self.kv_lora_rank), persistent=False)
             self.register_buffer("pe_cache", torch.zeros(args.max_batch_size, args.max_seq_len, self.n_local_heads, self.kv_lora_rank), persistent=False)
+            
+        # forward function
+        def forward(self, x: torch.Tensor, start_pos: int, freqs_cis: torch.Tensor, mask: Optional[torch.Tensor]):
+            """ 
+            Forward pass for the Multi-Headed Attention Layer (MLA)
+            
+            Args:
+                x (torch.Tensor): Input tensor of shape (batch_size, seq_len, dim).
+                start_pos (int): Starting position in the sequence for caching.
+                freqs_cis (torch.Tensor): Precomputed complex exponential values for rotary embeddings.
+                mask (Optional[torch.Tensor]): Mask tensor to exclude certain positions from attention.
+                
+            Returns:
+                torch.Tensor: Output tensor with the same shape as the input.
+            """
+            bsz, seqlen, _ = x.size()
+            end_pos = start_pos + seqlen
+            if self.q_lora_rank == 0:
+                q = self.wq(x)
+            else:
+                q = self.wq_b(self.q_norm(self.wq_a(x)))
+            q = q.view(bsz, seqlen, self.n_local_heads, self.qk_head_dim)
+            q_nope, q_pe = torch.split(q, [self.qk_nope_head_dim, self.qk_rope_head_dim, dim=-1])
+            q_pe = apply_rotary_emb(q_pe, freqs_cis)
+            kv = self.wkv_a(x)
+            kv, k_pe = torch.split(kv, [self.kv_lora_rank, self.qk_rope_head_dim, dim=-1])
+            k_pe = apply_rotary_emb(k_pe.unsqueeze(2), freqs_cis)
+            if attn_impl == "naive":
+                q = torch.cat([q_nope, q_pe], dim=-1)
+                kv = self.wkv_b(self.kv_norm(kv))
+                kv = kv.view(bsz, seqlen, self.n_local_heads, self.qk_nope_head_dim + self.v_head_dim)
+                k_nope, v = torch.split(kv, [self.qk_nope_head_dim, self.v_head_dim], dim=-1)
+                k = torch.cat([k_nope, k_pe.expand(-1,-1, self.n_local_heads, -1)])
+                self.k_cache[:bsz, start_pos:end_pos] = k
+                self.v_cache[:bsz, start_pos:end_pos] = v
+                scores = torch.einsum("bshd,bthd->bsht", q, self.k_cache[:bsz, :end_pos]) * self.softmax_scale
+            else:
+                # not naive implementation but instead deepseeks implementation
+                wkv_b = self.wkv_b.weight if self.wkv_b.scale is None else weight_dequant(self.wkv_b.weight, self.wkv_b.scale, block_size)
+                wkv_b = wkv_b.view(self.n_local_heads, -1, self.kv_lora_rank)
+                q_nope = torch.einsum("bshd,hdc->bshc", q_nope, wkv_b[:, :self.qk_nope_head_dim]) # transform merge
+                self.kv_cache[:bsz, start_pos:end_pos] = self.kv_norm(kv) # save normalized key value into cache
+                self.pe_cache[:bsz, start_pos:end_pos] = k_pe.squeeze(2) # same with positional
+                scores = (torch.einsum("bshc,btc->bsht", q_nope, self.kv_cache[:bsz, :end_pos]) +
+                        torch.einsum("bshr,btr->bsht", q_pe, self.pe_cache[:bsz, :end_pos])) * self.softmax_scale
+            if mask is not None:
+                scores += mask.unsqueeze(1)
+            scores = scores.softmax(dim=-1, dtype=torch.float32).type_as(x)
+            if attn_impl == "naive":
+                x = torch.einsum("bsht,bthd->bshd", scores, self.v_cache[:bsz, :end_pos])
+            else:
+                x = torch.einsum("bsht,btc->bshc", scores, self.kv_cache[:bsz, :end_pos])
+                x = torch.einsum("bshc,hdc->bshd", x, wkv_b[:, -self.v_head_dim:])
+            x = self.wo(x.flatten(2))
+            return x
+
